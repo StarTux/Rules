86ca2b65319eb3f522f8ac08994662edcfa72ed6
StarTux <ths.loy@googlemail.com>
2018-10-05 21:49:47 +0200

    Update with better looks

--- src/main/java/com/winthier/rules/RulesPlugin.java
+++ src/main/java/com/winthier/rules/RulesPlugin.java
@@ -1,32 +1,38 @@
 package com.winthier.rules;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.io.InputStreamReader;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
-import java.util.UUID;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.metadata.MetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class RulesPlugin extends JavaPlugin {
+public final class RulesPlugin extends JavaPlugin implements Listener {
     private final long privKey = new Random(System.currentTimeMillis()).nextLong();
     private static final String META_PASSWORD = "rules.password";
     private List<String> rules;
+    private ConfigurationSection messagesConfig;
+
+    // -- Plugin overrides
 
     @Override
     public void onEnable() {
         reloadConfig();
-        saveDefaultConfig();
-        saveResource("rules.yml", false);
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
@@ -41,21 +47,15 @@ public final class RulesPlugin extends JavaPlugin {
             if (playerInFromGroup(player)) {
                 if (getPassword(player).equalsIgnoreCase(args[1])) {
                     promotePlayer(player);
-                    Msg.send(player, "You have been promoted to &a%s&r!", getToGroup());
+                    player.sendMessage(getMessagesConfig().getString("Promotion"));
                 } else {
                     showRules(player);
                 }
-            } else {
-                Msg.warn(player, "You already accepted the rules.");
             }
         } else if (firstArg.equals("decline")) {
             Player player = sender instanceof Player ? (Player)sender : null;
             if (player == null) return false;
-            if (playerInFromGroup(player)) {
-                player.kickPlayer("You mush accept the rules.");
-            } else {
-                Msg.warn(player, "You already accepted the rules.");
-            }
+            player.kickPlayer(getMessagesConfig().getString("Decline"));
         } else if (firstArg.equals("pwof") && args.length == 2) {
             if (!sender.hasPermission("rules.admin")) return false;
             String targetName = args[1];
@@ -65,21 +65,43 @@ public final class RulesPlugin extends JavaPlugin {
                 return true;
             }
             String password = getPassword(target);
-            Msg.send(sender, "Password of %s: '%s'", target.getName(), password);
+            sender.sendMessage("Password of " + target.getName() + ": '" + password + "'");
         } else if (firstArg.equals("save") && args.length == 1) {
             if (!sender.hasPermission("rules.admin")) return false;
             saveDefaultConfig();
-            sender.sendMessage("[Rules] Default configuration saved.");
+            saveResource("rules.yml", false);
+            sender.sendMessage("[Rules] Default configurations saved.");
         } else if (firstArg.equals("reload") && args.length == 1) {
             if (!sender.hasPermission("rules.admin")) return false;
             reloadConfig();
             rules = null;
+            messagesConfig = null;
             sender.sendMessage("[Rules] Configuration reloaded.");
+        } else if (firstArg.equals("welcome") && args.length == 2) {
+            if (!sender.hasPermission("rules.admin")) return false;
+            Player target = getServer().getPlayerExact(args[1]);
+            if (target == null) {
+                sender.sendMessage("Player not found: " + args[1]);
+                return true;
+            }
+            sendWelcomeMessage(target);
+            sender.sendMessage("Welcome message sent to " + target.getName());
         } else {
             return false;
         }
         return true;
     }
+
+    ConfigurationSection getMessagesConfig() {
+        if (messagesConfig == null) {
+            YamlConfiguration cfg = YamlConfiguration.loadConfiguration(new File(getDataFolder(), "messages.yml"));
+            cfg.setDefaults(YamlConfiguration.loadConfiguration(new InputStreamReader(getResource("messages.yml"))));
+            messagesConfig = cfg;
+        }
+        return messagesConfig;
+    }
+
+    // --- Utility
 
     public String getPassword(Player player) {
         for (MetadataValue meta: player.getMetadata(META_PASSWORD)) {
@@ -104,31 +126,25 @@ public final class RulesPlugin extends JavaPlugin {
         return password;
     }
 
-    String getFromGroup() {
-        return getConfig().getString("FromGroup", "Guest");
-    }
-
-    String getToGroup() {
-        return getConfig().getString("ToGroup", "Friendly");
-    }
-
     void showRules(CommandSender sender) {
         Player player = sender instanceof Player ? (Player)sender : null;
         if (player != null) player.sendMessage("");
-        Msg.send(sender, "&a&lRules");
         for (String line: getRules()) {
-            Msg.send(sender, " %s", Msg.format(line));
+            sender.sendMessage(line);
         }
-        if (player != null) {
+        if (player != null && playerInFromGroup(player)) {
             player.sendMessage("");
-            if (playerInFromGroup(player)) {
-                List<Object> json = new ArrayList<>();
-                json.add(" Click here: ");
-                json.add(Msg.button(ChatColor.GREEN, "[Accept]", "Accept the rules", "/rules accept " + getPassword(player)));
-                json.add(" ");
-                json.add(Msg.button(ChatColor.DARK_RED, "[Decline]", "Decline the rules", "/rules decline"));
-                Msg.raw(player, json);
-            }
+            ComponentBuilder cb = new ComponentBuilder("");
+            cb.append(" Click here: ");
+            cb.append("[Accept]").color(ChatColor.GREEN)
+                .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText("Accept the rules")))
+                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/rules accept " + getPassword(player)));
+            cb.append(" or ").reset();
+            cb.append("[Decline]").color(ChatColor.RED)
+                .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText("Decline the rules")))
+                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/rules decline"));
+            player.spigot().sendMessage(cb.create());
+            player.sendMessage("");
         }
     }
 
@@ -145,8 +161,28 @@ public final class RulesPlugin extends JavaPlugin {
     List<String> getRules() {
         if (rules == null) {
             YamlConfiguration config = YamlConfiguration.loadConfiguration(new File(getDataFolder(), "rules.yml"));
+            config.setDefaults(YamlConfiguration.loadConfiguration(new InputStreamReader(getResource("rules.yml"))));
             rules = config.getStringList("Rules");
         }
         return rules;
+    }
+
+    void sendWelcomeMessage(Player player) {
+        for (List<String> ls: (List<List<String>>)getMessagesConfig().getList("Welcome")) {
+            ComponentBuilder cb = new ComponentBuilder(ls.get(1))
+                .color(ChatColor.valueOf(ls.get(0)))
+                .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/rules"))
+                .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText("/rules")));
+            player.spigot().sendMessage(cb.create());
+        }
+    }
+
+    // --- Event Handling
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        if (!playerInFromGroup(player)) return;
+        getServer().getScheduler().runTaskLater(this, () -> sendWelcomeMessage(player), 60L);
     }
 }
